c532a5747dda1ead0b2c845a966521e65567b8fc
fd-aguzinski <adrian.guzinski@fanduel.com>
2017-06-19 16:18:55 +0100

    Added About page

--- src/about/About.js
+++ src/about/About.js
@@ -9,8 +9,16 @@ class About extends Component {
 
     render() {
         return (
-            <div className="account">
-                About!
+            <div className="about">
+                <div className="about-container">
+                    <div className="about-container__header">
+                        About
+                    </div>
+                    <hr className="faded"/>
+                    <div className="about-container__content">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque aliquet mi sed enim porttitor, ut aliquam turpis feugiat. Ut gravida consectetur molestie. Mauris eu molestie massa, dictum facilisis erat. Nunc vel fringilla tortor. Nam in neque id nulla lacinia consectetur. Vivamus cursus semper quam nec feugiat. Aenean mattis fringilla velit a malesuada. Duis volutpat nibh neque, vel pretium odio porttitor in. Donec molestie erat at velit consequat, at pretium urna gravida. Nam vitae sagittis sem. Sed laoreet erat dui, at eleifend nulla viverra ac.
+                    </div>
+                </div>
             </div>
         );
     }
